1e5224badc96a479ee08e84ed2ad98ff99724f89
Mark Parnell <markparnell@brownbox.net.au>
2018-01-03 16:23:00 +1100

    Update to slick 1.8.0

--- theme/scripts.php
+++ theme/scripts.php
@@ -28,7 +28,7 @@ class bb_enqueue {
         // Theme styles
         wp_enqueue_style('theme_style', get_stylesheet_directory_uri().'/css/style.css', array(), filemtime(get_stylesheet_directory().'/css/style.css'));
         wp_enqueue_style('theme_dynamic', get_stylesheet_directory_uri().'/css/'.bb_get_dynamic_styles_filename(), array(), filemtime(get_stylesheet_directory().'/css/'.bb_get_dynamic_styles_filename()));
-        wp_enqueue_style('slick', get_stylesheet_directory_uri().'/css/vendor/slick.css', array(), '1.5.8');
+        wp_enqueue_style('slick', get_stylesheet_directory_uri().'/css/vendor/slick.css', array(), '1.8.0');
 
         // Header scripts
 //         wp_enqueue_script('fontawesome', get_template_directory_uri().'/js/fontawesome-all.min.js', array(), '5.0.2'); Pro version
